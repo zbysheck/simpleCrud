ada63a495190821a2cdae88b9569abb9412ae39d
Zbyszek Kisły <zbyshekh@gmail.com>
2017-09-29 17:56:43 +0200

    remove and extraction of the curl ommunicating with api

--- src/AppBundle/Controller/StationController.php
+++ src/AppBundle/Controller/StationController.php
@@ -6,33 +6,34 @@ use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
 use Symfony\Component\Debug\Debug;
 use Symfony\Component\DependencyInjection\ContainerInterface;
+use Symfony\Component\HttpFoundation\Request;
 
 class StationController extends Controller
 {
-    /**
-     * @Route("/list")
-     */
-    public function listAction()
-    {
-        Debug::enable();
-        $url = 'https://api.feedback.quantumlab.co/v1';
-        $key = "c648c405-af1f-4286-b7fb-47eec4e7e565";
-        $myvars = '{     "jsonrpc": "2.0",     "method": "station_list",     "params": {            "key": "'. $key .'"     },     "id": 1 }';
+    private $url = 'https://api.feedback.quantumlab.co/v1';
+    private $key = "c648c405-af1f-4286-b7fb-47eec4e7e565";
 
-        $ch = curl_init( $url );
+    function curlIt($json){
+        $ch = curl_init( $this->url );
         curl_setopt( $ch, CURLOPT_POST, 1);
-        curl_setopt( $ch, CURLOPT_POSTFIELDS, $myvars);
+        curl_setopt( $ch, CURLOPT_POSTFIELDS, $json);
         curl_setopt( $ch, CURLOPT_FOLLOWLOCATION, 1);
         curl_setopt( $ch, CURLOPT_HEADER, 0);
         curl_setopt( $ch, CURLOPT_RETURNTRANSFER, 1);
 
         $response = curl_exec( $ch );
+        return json_decode($response);
+    }
 
-        $response = json_decode($response);
-
-        dump($response->result);
+    /**
+     * @Route("/list")
+     */
+    public function listAction()
+    {
+        $json = '{     "jsonrpc": "2.0",     "method": "station_list",     "params": {            "key": "'. $this->key .'"     },     "id": 1 }';
+        $result = $this->curlIt($json)->result;
         return $this->render('AppBundle:Station:list.html.twig', array(
-            "stations" => $response->result
+            "stations" => $result
         ));
     }
 
@@ -41,63 +42,71 @@ class StationController extends Controller
      */
     public function addAction()
     {
-
-        $url = 'https://api.feedback.quantumlab.co/v1';
-        $myvars = '{     "jsonrpc": "2.0",     "method": "station_list",     "params": {         "key": "c648c405-af1f-4286-b7fb-47eec4e7e565"     },     "id": 1 }';
-
-        $ch = curl_init( $url );
-        curl_setopt( $ch, CURLOPT_POST, 1);
-        curl_setopt( $ch, CURLOPT_POSTFIELDS, $myvars);
-        curl_setopt( $ch, CURLOPT_FOLLOWLOCATION, 1);
-        curl_setopt( $ch, CURLOPT_HEADER, 0);
-        curl_setopt( $ch, CURLOPT_RETURNTRANSFER, 1);
-
-        $response = curl_exec( $ch );
-
-        $response = json_decode($response);
-
-        //  var_dump($response->result);
+        $json = '{     "jsonrpc": "2.0",     "method": "location_list",     "params": {         "key": "'.$this->key.'"     },     "id": 1 }';
+        $result = $this->curlIt($json)->result;
         return $this->render('AppBundle:Station:add.html.twig', array(
-
+            "locations" => $result
         ));
 
-
     }
-    /**
-    * @Route("/locations")
-    */
-    public function locationsAction()
-    {
 
-        $url = 'https://api.feedback.quantumlab.co/v1';
-        $key = "c648c405-af1f-4286-b7fb-47eec4e7e565";
-        $myvars = '
-        {
+    /**
+     * @Route("/add/submit")
+     * @param Request $request
+     * @return \Symfony\Component\HttpFoundation\Response
+     */
+    public function addSubmitAction(Request $request)
+    {
+        $name = $request->request->get("name");
+        $location = $request->request->get("location");
+        $json = '{
             "jsonrpc": "2.0",
-            "method": "location_list",
+            "method": "station_create",
             "params": {
-                "key": "'. $key .'"
+                "key": "'.$this->key.'",
+            "locationUuid": "'.$location.'",
+            "metaData": {
+                "name": "'.$name.'"
+            }
             },
             "id": 1
         }';
-
-        $ch = curl_init( $url );
-        curl_setopt( $ch, CURLOPT_POST, 1);
-        curl_setopt( $ch, CURLOPT_POSTFIELDS, $myvars);
-        curl_setopt( $ch, CURLOPT_FOLLOWLOCATION, 1);
-        curl_setopt( $ch, CURLOPT_HEADER, 0);
-        curl_setopt( $ch, CURLOPT_RETURNTRANSFER, 1);
-
-        $response = curl_exec( $ch );
-
-        $response = json_decode($response);
-
-          var_dump($response->result);
+        $json = '{     "jsonrpc": "2.0",     "method": "location_list",     "params": {         "key": "'.$this->key.'"     },     "id": 1 }';
+        $result = $this->curlIt($json)->result;
         return $this->render('AppBundle:Station:add.html.twig', array(
-
+            "locations" => $result
         ));
 
+    }
+
+    /**
+     * @Route("/remove")
+     * @param Request $request
+     * @return \Symfony\Component\HttpFoundation\Response
+     */
+    public function removeAction(Request $request)
+    {
+        $uuid = $request->request->get("uuid");
+        $json = '{
+            "jsonrpc": "2.0",
+            "method": "station_remove",
+            "params": {
+                "key": "'.$this->key.'",
+                "uuid": "'.$uuid.'"
+            },
+            "id": 1
+        }';
+        $result = $this->curlIt($json);
+        $json = '{     "jsonrpc": "2.0",     "method": "station_list",     "params": {            "key": "'. $this->key .'"     },     "id": 1 }';
+        $result = $this->curlIt($json)->result;
+        return $this->render('AppBundle:Station:list.html.twig', array(
+            "stations" => $result
+        ));
 
     }
+
+    
+
+
 
 }
